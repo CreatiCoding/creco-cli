432b915b95c13172683c0418c812b7adbb951e77
CreatiCoding <creaticoding@gmail.com>
2021-06-27 17:57:13 +0900

    feat: update npm version

---
+++ scripts/update.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+version=$(node -p 'require("./package.json").version.split(".").map((e, i) => i === 2 ? `${parseInt(e + 1)}` : e).join(".")')
+git add ./package.json
+echo $version
+npm version $version || echo "Git working directory 가 비어있지 않아 NPM version이 update 되지 않았습니다."
